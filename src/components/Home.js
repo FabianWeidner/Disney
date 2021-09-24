6177f344286666fe2c20a421c734f3f4d8f74449
Fabian Phillipe <fabianphillipe@gmail.com>
2021-09-24 16:32:56 +0200

    add slider with react slick

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,11 +16,12 @@ const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position: relative;
+  overflow-x: hidden;
 
   &:before {
     background: url('/images/home-background.png') center center / cover
       no-repeat fixed;
-    content: 'yooo';
+    content: '';
     position: absolute;
     top: 0;
     left: 0;
